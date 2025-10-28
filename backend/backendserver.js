ae91bb32f379a2878488217a62bcf89b556ac81b
Noman Khan <codenk157@gmail.com>
2025-10-29 02:44:30 +0500

    Add basic backend express server

---
+++ backend/backendserver.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Backend Running ✅");
+});
+
+app.listen(3001, () => console.log("Server running on port 3001"));
